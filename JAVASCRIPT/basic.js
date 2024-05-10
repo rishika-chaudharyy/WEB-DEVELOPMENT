3f27e39ae7f879a8a83e339819f3120f44a1c8ca
RISHIKA CHAUDHARY <rishikachaudharyyy@gmail.com>
2024-05-10 13:24:07 +0530

    Add files via upload

---
+++ JAVASCRIPT/basic.js
@@ -0,0 +1,63 @@
+console.log("Hello World, Welcome to console");
+console.log("Let's learn JS together");
+
+var name = "Tony Stark";
+console.log(name);
+
+var price = 99.99;
+console.log(price);
+
+var c = null;
+console.log(c);
+
+var y = undefined;
+console.log(y);
+
+var isFollow = false;
+console.log(isFollow);
+
+var fullname = "Rishika";
+var FULLNAME = "RISHIKA";
+console.log(fullname);
+console.log(FULLNAME);
+
+console.log(typeof price);
+
+let x = BigInt("123");
+console.log(x);
+
+const student = {
+    firstName: "Rahul",
+    age: 70,
+    cgpa: 9.00,
+    college: "nirma"
+};
+
+console.log(student); // Logging the student object
+console.log(student["firstName"])
+console.log(student.age)
+console.log(student["cgpa"])
+
+
+//practice question
+//Q.1
+
+const product={
+    title:"Parker ball pen",
+    offer:5,
+    rating:4.0,
+    hasDeal:true,
+    price:270
+};
+
+console.log(product);
+
+
+const profile={
+    userId:"Rishika",
+    followers:597000,
+    posts:107,
+    following:4,
+    thread:"@Rishika"
+};
+console.log(profile)
