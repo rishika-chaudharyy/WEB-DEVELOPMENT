6b11c5fdd31ef38cb7fd8038a8b23070f3ab8173
RISHIKA CHAUDHARY <rishikachaudharyyy@gmail.com>
2024-06-02 14:32:40 +0530

    Add files via upload

---
+++ DOM JAVASCRIPT/script.js
@@ -0,0 +1,29 @@
+let heading = document.getElementById("heading");
+console.dir(heading);
+
+let elems = document.getElementsByClassName("myclass");
+console.dir(elems);
+
+let para = document.getElementsByTagName("p");
+console.dir(para);
+
+let firstelem = document.querySelector("p");
+console.dir(firstelem);
+
+let allelem = document.querySelectorAll(".myclass");
+console.dir(allelem);
+
+console.log(firstelem.tagName);
+
+console.log(document.body.firstChild)
+
+let res=document.querySelectorAll("#mydiv").children
+console.log(res)
+
+let text=document.querySelector("h2");
+console.dir(text)
+text.innerText=text.innerText+" From Apna College Students"
+
+let divs=document.querySelectorAll(".box")
+console.dir(divs)
+divs[0].innerText="new value"
