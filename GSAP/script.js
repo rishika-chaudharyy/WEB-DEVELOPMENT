69467d38be2b484fe2ac5de2ca50c05e5da7dfe2
RISHIKA CHAUDHARY <rishikachaudharyyy@gmail.com>
2024-08-17 00:39:16 +0530

    Add files via upload

--- GSAP/script.js
+++ GSAP/script.js
@@ -208,13 +208,148 @@
 //     }
 // })
 
-gsap.to("#page2 h1",{
-    transform:"translate(-150%)",
-    scrollTrigger:{
-        trigger:"#page2 h1",
-        scroller:"body",
-        markers:true,
-        start:"top 50%",
+// gsap.to("#page2 h1", {
+//     transform: "translate(-150%)",
+//     scrollTrigger: {
+//         trigger: "#page2",
+//         scroller: "body",
+//         markers: true,
+//         start: "top 0%",
+//         end: "top -100%",
+//         scrub: 2,
+//         pin: true
+//     }
+// })
 
-    }
-})
+
+// SVG
+
+// var path = document.getElementById("path");
+// var initialValue = `M 10 100 Q 500 100 990  100`;
+// var finalPath = `M 10 100 Q 500 100 990  100`;
+
+// document.getElementById("string").addEventListener("mousemove", function (e) {
+//     finalPath = `M 10 100 Q ${e.clientX} ${e.clientY} 990  100`;
+//     gsap.to(path, {
+//         duration: 0.3,
+//         attr: { d: finalPath },
+//         ease: "power3.out"
+//     });
+// });
+
+// document.getElementById("string").addEventListener("mouseleave", function () {
+//     gsap.to(path, {
+//         duration: 0.5,
+//         attr: { d: initialValue },
+//         ease: "elastic.out(1,0.2)"
+//     });
+// });
+
+
+
+//CUSTOM CURSOR
+
+// let main = document.querySelector("#main");
+// let cursor = document.querySelector("#cursor");
+// let imageDiv = document.querySelector("#image");
+
+// main.addEventListener("mousemove", function (e) {
+//     gsap.to(cursor, {
+//         x: e.x,
+//         y: e.y,
+//         // ease: "back.out"
+//     })
+// })
+
+// imageDiv.addEventListener("mouseenter", function () {
+//     cursor.innerHTML="View More"
+//     gsap.to(cursor,{
+//         scale:4
+//     })
+// })
+
+// imageDiv.addEventListener("mouseleave", function () {
+//     cursor.innerHTML=""
+//     gsap.to(cursor,{
+//         scale:1
+//     })
+// })
+
+
+// var menu = document.querySelector("#nav i");
+// var cross = document.querySelector("#full i");
+
+// var tl = gsap.timeline()
+
+// tl.to("#full", {
+//     right: 0,
+//     duration: 0.6
+// })
+
+// tl.from("#full h4", {
+//     x: 150,
+//     duration: 0.6,
+//     stagger: 0.2,
+//     opacity: 0
+// })
+
+// tl.from("#full i", {
+//     opacity: 0
+// })
+
+// tl.pause();
+
+// menu.addEventListener("click", function () {
+//     tl.play();
+// })
+
+// cross.addEventListener("click", function () {
+//     tl.reverse();
+// })
+
+
+
+// function breakText() {
+//     let h1 = document.querySelector("h1");
+//     let h1Text = document.querySelector("h1").textContent;
+
+//     let splittedText = h1Text.split("");
+
+//     var clutter = ""
+
+//     var halfValue = Math.floor(splittedText.length) / 2
+
+
+//     splittedText.forEach(function (e, idx) {
+
+//         if (idx < halfValue) {
+//             clutter = clutter + `<span class="a">${e}</span>`
+//         }else{
+//             clutter = clutter + `<span class="b">${e}</span>`
+//         }
+
+       
+//     })
+
+//     h1.innerHTML=clutter;
+
+
+// }
+
+// breakText();
+
+// gsap.from("h1 .a", {
+//     y: 80,
+//     duration: 0.6,
+//     delay: 0.5,
+//     opacity: 0,
+//     stagger: 0.15
+// })
+
+// gsap.from("h1 .b", {
+//     y: 80,
+//     duration: 0.6,
+//     delay: 0.5,
+//     opacity: 0,
+//     stagger: -0.15
+// })
